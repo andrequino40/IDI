f6bd6ff84b8ac3dc444f24830bc6fe5a00742f22
angel framework <andrequino40@gmail.com>
2025-04-19 18:12:23 +0200

    Creado s2.3

---
+++ S2.3/MyGLWidget.cpp
@@ -0,0 +1,307 @@
+// MyGLWidget.cpp
+#include "MyGLWidget.h"
+#include <iostream>
+#include <stdio.h>
+
+#define printOpenGLError() printOglError(__FILE__, __LINE__)
+#define CHECK() printOglError(__FILE__, __LINE__,__FUNCTION__)
+#define DEBUG() std::cout << __FILE__ << " " << __LINE__ << " " << __FUNCTION__ << std::endl;
+
+int MyGLWidget::printOglError(const char file[], int line, const char func[]) 
+{
+    GLenum glErr;
+    int    retCode = 0;
+
+    glErr = glGetError();
+    const char * error = 0;
+    switch (glErr)
+    {
+        case 0x0500:
+            error = "GL_INVALID_ENUM";
+            break;
+        case 0x501:
+            error = "GL_INVALID_VALUE";
+            break;
+        case 0x502: 
+            error = "GL_INVALID_OPERATION";
+            break;
+        case 0x503:
+            error = "GL_STACK_OVERFLOW";
+            break;
+        case 0x504:
+            error = "GL_STACK_UNDERFLOW";
+            break;
+        case 0x505:
+            error = "GL_OUT_OF_MEMORY";
+            break;
+        default:
+            error = "unknown error!";
+    }
+    if (glErr != GL_NO_ERROR)
+    {
+        printf("glError in file %s @ line %d: %s function: %s\n",
+                             file, line, error, func);
+        retCode = 1;
+    }   
+    return retCode;
+}
+
+void MyGLWidget::modelTransformSuelo() 
+{
+  // Matriu de transformació de model
+  glm::mat4 transform (1.0f);
+  transform = glm::scale(transform, glm::vec3(escala));
+  glUniformMatrix4fv(transLoc, 1, GL_FALSE, &transform[0][0]);
+}
+
+void MyGLWidget::modelTransformModel () 
+{
+  // Matriu de transformació de model
+  glm::mat4 transform (1.0f);
+  transform = glm::rotate (transform, (float)glm::radians(rota_model), glm::vec3 (0.0, 1.0, 0.0));
+  transform = glm::scale(transform, glm::vec3(factor_escala_model));
+  transform = glm::translate(transform, -centre_base);
+  glUniformMatrix4fv(transLoc, 1, GL_FALSE, &transform[0][0]);
+}
+
+void MyGLWidget::keyPressEvent(QKeyEvent* event) 
+{
+  makeCurrent();
+  switch (event->key()) {
+    case Qt::Key_S: { // escalar a més gran
+      escala_desitjada_model+= 0.05;
+      calculaCapsaModel(m, factor_escala_model, escala_desitjada_model, centre_base);
+      break;
+    }
+    case Qt::Key_D: { // escalar a més petit
+      escala_desitjada_model-= 0.05;
+      calculaCapsaModel(m, factor_escala_model, escala_desitjada_model, centre_base);
+      break;
+    }
+    case Qt::Key_Q: {
+      rota_model += 45.0;
+      break;
+    }
+    case Qt::Key_E: {
+      rota_model -= 45.0;
+      break;
+    }
+    default: event->ignore(); break;
+  }
+  update();
+}
+
+void MyGLWidget::resizeGL (int width, int height){
+  rav = float (width) / float (height);
+  updateCamera();
+}
+
+void MyGLWidget::paintGL () 
+{
+// En cas de voler canviar els paràmetres del viewport, descomenteu la crida següent i
+// useu els paràmetres que considereu (els que hi ha són els de per defecte)
+
+  // glViewport (0, 0, ample, alt);
+  // std::cerr << "ar: " <<  << std::endl; 
+  // std::cerr << width() << " " << height() << std::endl; 
+  // std::cerr << ">" << ample << " " << alt << std::endl; 
+  // Esborrem el frame-buffer
+  glClear (GL_COLOR_BUFFER_BIT);
+  glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  // Carreguem la transformació de model
+  modelTransformModel();
+
+  // Activem el VAO per a pintar la caseta 
+  glBindVertexArray(VAO_HomerProves);
+
+  // pintem
+  glDrawArrays (GL_TRIANGLES, 0, m.faces ().size () * 3);
+
+  // repeat
+  modelTransformSuelo();
+
+  glBindVertexArray(VAO_Suelo);
+
+  glDrawArrays(GL_TRIANGLES, 0, vertices_Suelo);
+
+  glBindVertexArray (0);
+}
+
+
+void MyGLWidget::creaBuffers(){
+   creaBuffersModel();
+   creaBuffersSuelo();
+}
+
+void MyGLWidget::creaBuffersModel(){
+    // lodea modelo
+    m.load("./Model/Patricio.obj");
+
+    calculaCapsaModel(m, factor_escala_model, escala_desitjada_model, centre_base);
+
+    // crea vao
+    glGenVertexArrays(1, &VAO_HomerProves);
+    glBindVertexArray(VAO_HomerProves);
+
+    // crea vbos
+    GLuint VBO_Homer[2];
+
+    glGenBuffers(2, VBO_Homer);
+
+    // carga vbo vertices
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_Homer[0]);
+    glBufferData (GL_ARRAY_BUFFER, sizeof(GLfloat) * m.faces ().size () * 3 * 3, m.VBO_vertices(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(vertexLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(vertexLoc);
+
+    // carga vbo colores
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_Homer[1]);
+    glBufferData (GL_ARRAY_BUFFER, sizeof(GLfloat) * m.faces ().size () * 3 * 3, m.VBO_matdiff(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(colorLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(colorLoc);
+
+    glBindVertexArray (0);
+}
+
+
+
+void MyGLWidget::creaBuffersSuelo() {
+    glm::vec3 Vertices[vertices_Suelo];  // Tres vèrtexs amb X, Y i Z
+    int i = 0;
+// lado frente izquierda
+  Vertices[i++] = glm::vec3(-2.5, 0.0, -2.5);
+  Vertices[i++] = glm::vec3(-2.5, 0.0, 2.5);
+  Vertices[i++] = glm::vec3(2.5, 0.0, 2.5);
+  // lade fondo derecha
+  Vertices[i++] = glm::vec3(2.5, 0.0, 2.5);
+  Vertices[i++] = glm::vec3(2.5, 0.0, -2.5);
+  Vertices[i++] = glm::vec3(-2.5, 0.0, -2.5);
+  
+  // Creació del Vertex Array Object (VAO) que usarem per pintar
+  glGenVertexArrays(1, &VAO_Suelo);
+  glBindVertexArray(VAO_Suelo);
+
+  // Creació del buffer amb les dades dels vèrtexs
+  GLuint VBO1;
+  glGenBuffers(1, &VBO1);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO1);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+  // Activem l'atribut que farem servir per vèrtex (només el 0 en aquest cas)	
+  glVertexAttribPointer(vertexLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(vertexLoc);
+
+  //VBO2 
+  glm::vec3 Colors[vertices_Suelo];
+  i = 0;
+  Colors[i++] = glm::vec3(0.67, 0.85, 0.9);   // LBlue
+  Colors[i++] = glm::vec3(1.0, 0.75, 0.8);  // Pink
+Colors[i++] = glm::vec3(0.67, 0.85, 0.9);   // LBlue 
+  Colors[i++] = glm::vec3(0.67, 0.85, 0.9);   // LBlue
+  Colors[i++] = glm::vec3(1.0, 0.75, 0.8);  // Pink
+  Colors[i++] = glm::vec3(0.67, 0.85, 0.9);   // LBlue
+ 
+   // Creació del buffer amb les dades dels vèrtexs
+  GLuint VBO2;
+  glGenBuffers(1  , &VBO2);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(Colors), Colors, GL_STATIC_DRAW);
+  // Activem l'atribut que farem servir per vèrtex (només el 0 en aquest cas)	
+  glVertexAttribPointer(colorLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(colorLoc);
+
+  // Desactivem el VAO
+  glBindVertexArray(0);
+}
+void MyGLWidget::initializeGL()
+{
+  // Cal inicialitzar l'ús de les funcions d'OpenGL
+  BL2GLWidget::initializeGL();
+  updateCamera();
+  // MyGLWidget::projectTransform();
+  // MyGLWidget::viewTransform();
+  glEnable (GL_DEPTH_TEST);
+}
+
+void MyGLWidget::calculaCapsaModel (Model &p, float &escala, float alcadaDesitjada, glm::vec3 &centreBase)
+{
+  // Càlcul capsa contenidora i valors transformacions inicials
+  float minx, miny, minz, maxx, maxy, maxz;
+  minx = maxx = p.vertices()[0];
+  miny = maxy = p.vertices()[1];
+  minz = maxz = p.vertices()[2];
+  for (unsigned int i = 3; i < p.vertices().size(); i+=3)
+  {
+    if (p.vertices()[i+0] < minx)
+      minx = p.vertices()[i+0];
+    if (p.vertices()[i+0] > maxx)
+      maxx = p.vertices()[i+0];
+    if (p.vertices()[i+1] < miny)
+      miny = p.vertices()[i+1];
+    if (p.vertices()[i+1] > maxy)
+      maxy = p.vertices()[i+1];
+    if (p.vertices()[i+2] < minz)
+      minz = p.vertices()[i+2];
+    if (p.vertices()[i+2] > maxz)
+      maxz = p.vertices()[i+2];
+  }
+
+  escala = alcadaDesitjada/(maxy-miny);
+  centreBase[0] = (minx+maxx)/2.0; centreBase[1] = miny; centreBase[2] = (minz+maxz)/2.0;
+}
+
+void MyGLWidget::carregaShaders() { // declarem-lo també en MyGLWidget.h
+    BL2GLWidget::carregaShaders(); // cridem primer al mètode de BL2GLWidget
+    projLoc = glGetUniformLocation (program->programId(), "proj");
+    viewLoc = glGetUniformLocation (program->programId(), "view");
+}
+
+void MyGLWidget::projectTransform (float fov, float ra, float z_near, float z_far) {
+// glm::perspective (FOV en radians, ra window, znear, zfar)
+// float)glm::radians()
+    glm::mat4 Proj = glm::perspective (fov, ra, z_near, z_far);
+    glUniformMatrix4fv (projLoc, 1, GL_FALSE, &Proj[0][0]);
+}
+
+void MyGLWidget::viewTransform(glm::vec3 OBS, glm::vec3 VRP, glm::vec3 UP) {
+  glm::mat4 View = glm::lookAt(OBS, VRP, UP);
+  glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &View[0][0]);
+}
+
+void MyGLWidget:: calcAtributsEscena(glm::vec3 min, glm::vec3 max) {
+  centre_escena = (min+max)/2.0f;
+  radi_escena = glm::distance(centre_escena, max);
+}
+
+void MyGLWidget::updateCamera() {
+  // valores por defecto de la escena de homer y el suelo
+   calcAtributsEscena(glm::vec3(-2.5f, 0.0f, -2.5f), glm::vec3(2.5f, 4.0f, 2.5f));
+
+  float d = radi_escena * 2;
+
+  glm::vec3 VRP = centre_escena;
+  glm::vec3 OBS = VRP + glm::vec3(0,1,d);
+  glm::vec3 UP = glm::vec3(0,1,0);
+
+  viewTransform(OBS, VRP, UP);
+
+  float z_near = d - radi_escena;
+  float z_far = d + radi_escena;
+  float fov;
+  float av = glm::asin(radi_escena/d);
+  if (rav >= 1)
+    fov = 2 * av;
+  else 
+    fov = 2 * glm::atan((glm::tan(av)/rav));
+
+  projectTransform(fov, rav, z_near, z_far);
+}
+
+
+
+MyGLWidget::~MyGLWidget() {
+}
+
+
---
+++ S2.3/MyGLWidget.h
@@ -0,0 +1,45 @@
+// MyGLWidget.h
+#include "BL2GLWidget.h"
+#include "./Model/model.h"
+
+class MyGLWidget : public BL2GLWidget {
+  Q_OBJECT
+
+  public:
+    MyGLWidget(QWidget *parent=0) : BL2GLWidget(parent) {}
+    ~MyGLWidget();
+
+  protected:
+  void carregaShaders();
+  void initializeGL();
+  void creaBuffers ();
+  void paintGL ();
+  void keyPressEvent(QKeyEvent* event);
+  void resizeGL (int width, int height);
+  
+  
+  
+  GLuint projLoc, viewLoc;
+  Model m;
+  GLuint VAO_HomerProves, VAO_Suelo;
+  
+  private:
+  float rav = 1;
+  glm::vec3 centre_base;
+  float factor_escala_model;
+  float rota_model = 45.0;
+  float escala_desitjada_model = 4.0f;
+  int vertices_Suelo = 6;
+  glm::vec3 centre_escena;
+  float radi_escena;
+  void calculaCapsaModel (Model &p, float &escala, float alcadaDesitjada, glm::vec3 &CentreBase);
+  void updateCamera();
+  void calcAtributsEscena(glm::vec3 punt_min, glm::vec3 punt_max);
+  void modelTransformSuelo() ;
+  void modelTransformModel() ;
+  void creaBuffersSuelo();
+  void creaBuffersModel();
+  void viewTransform (glm::vec3 OBS, glm::vec3 VRP, glm::vec3 UP);
+  void projectTransform (float fov, float ra, float z_near, float z_far);
+  int printOglError(const char file[], int line, const char func[]);
+};
